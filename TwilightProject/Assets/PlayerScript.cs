f01bc7d0e317d9fc1f9b2712ec9f78a62e297043
Beelo9706 <bieloemerald@gmail.com>
2026-08-10 01:02:18 +0300

    BasicVisuals
    The basic visuals that will be used to test the prototype

---
+++ TwilightProject/Assets/PlayerScript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public Rigidbody2D myRigidbody;
+    public float playerSpeed;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+        
+    }
+}
